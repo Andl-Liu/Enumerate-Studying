a2bfe1a6c16c64ae54b2ea1683fffd95c95171fd
Andl <619246032@qq.com>
2021-01-25 20:09:22 +0800

    submit

---
+++ Enumerate/Enumerate/binary.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+void print_subset(int n, int s)
+{
+	for (int i = 0; i < n; i++) 
+	{
+		if (s & (1 << i)) {
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+}
+
+int main() 
+{
+	int n;
+	scanf("%d",&n);
+	for (int i = 0; i < (1 << n); i++)
+	{
+		print_subset(n, i);
+	}
+	return 0;
+}
